3d4eae5d2e15be8454f3a62b2f178beb9d956851
ademic2022 <adeeyomichael77@gmail.com>
2023-12-03 01:27:05 +0100

    react-default-files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import Sidenav from './components/Sidenav';
+import Main from './components/Main';
+import Skill from './components/Skill';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Sidenav />
+      <Main />
+      <Skill />
+      <Projects />
+      <Contact />
+    </>
   );
 }
 
