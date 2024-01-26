b4e5d0a802b7e73f9893d2b98ec90f89d158148e
ademic2022 <adeeyomichael77@gmail.com>
2024-01-26 02:02:38 +0100

    refactor: replace Lorem Ipsum with actual text

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,45 +1,50 @@
-import React from 'react';
-import SkillsItem from './SkillsItem';
+import React from "react";
+import SkillsItem from "./SkillsItem";
 
 const data = [
-    {
-        year:2020,
-        title:'Backend Developer',
-        duration:'2 Years',
-        details:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet accusantium, illo ipsam nostrum repellat quam deleniti at commodi beatae facere praesentium aut perspiciatis magni doloremque est, dolorem quo animi iste.'
-    },
-    {
-        year:2017,
-        title:'Software Engineer',
-        duration:'3 Years',
-        details:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet accusantium, illo ipsam nostrum repellat quam deleniti at commodi beatae facere praesentium aut perspiciatis magni doloremque est, dolorem quo animi iste.'
-    },
-    {
-        year:2015,
-        title:'Content Creator',
-        duration:'1 Years',
-        details:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet accusantium, illo ipsam nostrum repellat quam deleniti at commodi beatae facere praesentium aut perspiciatis magni doloremque est, dolorem quo animi iste.'
-    },
-    {
-        year:2012,
-        title:'Backend Developer',
-        duration:'2 Years',
-        details:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet accusantium, illo ipsam nostrum repellat quam deleniti at commodi beatae facere praesentium aut perspiciatis magni doloremque est, dolorem quo animi iste.'
-    }
-]
+  {
+    year: 2021,
+    title: "Backend Developer",
+    duration: "2 Years Experience",
+    details:
+      "As a Backend Developer, I've accumulated 2 years of valuable experience, crafting robust and scalable server-side solutions. My role involved implementing efficient APIs, optimizing database queries, and ensuring the seamless functioning of web applications.",
+  },
+  {
+    year: 2021,
+    title: "Software Engineer",
+    duration: "3 Years Experience",
+    details:
+      "With 3 years as a Software Engineer, I've been deeply involved in the development life cycle, from conceptualization to deployment. My expertise includes designing and implementing software solutions, collaborating with cross-functional teams, and continuously enhancing code quality.",
+  },
+  {
+    year: 2022,
+    title: "FrontEnd Developer",
+    duration: "2 Years",
+    details:
+      "As a FrontEnd Developer for 2 years, I specialized in crafting visually appealing and user-friendly interfaces. My role involved translating design concepts into code, optimizing website performance, and staying updated with the latest front-end technologies.",
+  },
+  {
+    year: 2015,
+    title: "Content Creator",
+    duration: "2 Years Experience",
+    details:
+      "During my 2-year stint as a Content Creator, I honed my skills in creating engaging and informative content. Whether it was writing articles, producing videos, or managing social media platforms, I successfully communicated complex concepts in an accessible manner.",
+  },
+];
 
 const Skill = () => {
   return (
-    <div id='skills' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16 '>
-        <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Skill</h1>
-        {data.map((item, idx)=>(
-            <SkillsItem 
-            key={idx} 
-            year={item.year} 
-            title={item.title} 
-            duration={item.duration} 
-            details={item.details} />
-        ))}
+    <div id="skills" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16 ">
+      <h1 className="text-4xl font-bold text-center text-[#001b5e]">Skill</h1>
+      {data.map((item, idx) => (
+        <SkillsItem
+          key={idx}
+          year={item.year}
+          title={item.title}
+          duration={item.duration}
+          details={item.details}
+        />
+      ))}
     </div>
   );
 };
